f27fc09e40fda80e3fc49abf13f04f52b643f6cd
bayer <bayer07b@gmail.com>
2019-07-25 22:58:21 +0700

    i am sorry, i do not have enough time for task, i need to prepare for traveling

---
+++ WebApplication/UnitTests/TestInitializer.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationLayer.Services;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class TestInitializer
+    {
+        [OneTimeSetUp]
+        public void Init()
+        {
+            
+        }
+
+        [OneTimeTearDown]
+        public void Dispose()
+        { /* ... */ }
+    }
+}
